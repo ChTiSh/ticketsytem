a3dc7f89483b1e2343fbfe8b0879f35e9412978b
ChTiSh <chentina.shen@gmail.com>
2024-03-12 22:41:57 -0700

    switch back to server

--- src/app/api/ticket-data/page.ts
+++ src/app/api/ticket-data/page.ts
@@ -1,36 +1,3 @@
-import { NextResponse } from 'next/server';
-import { db } from '../../../db/index';
-import { ticket } from '../../../db/ticketTable/schema';
-
-export const config = {
-  runtime: 'nodejs',
-};
-
-export default async function handler(req: Request) {
-  const method = req.method;
-
-  if (method === 'GET') {
-    try {
-      const tickets = await db.select().from(ticket);
-      return NextResponse.json(tickets, { status: 200 });
-    } catch (error) {
-      console.error('Error fetching tickets:', error);
-      return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-    }
-  } else if (method === 'POST') {
-    try {
-      const body = await req.text();
-      const data = JSON.parse(body);
-      const insertedTicket = await db.insert(ticket).values(data);
-      return NextResponse.json(insertedTicket, { status: 201 });
-    } catch (error) {
-      console.error('Error creating a ticket:', error);
-      return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-    }
-  } else {
-    return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
-  }
-}
 
 
 // import { NextApiRequest, NextApiResponse } from 'next';
