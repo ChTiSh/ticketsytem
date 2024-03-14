025a3a98713e73534352ebe6c6405c2da534c760
ChTiSh <chentina.shen@gmail.com>
2024-03-13 21:51:09 -0700

    fix the server routing issue, finally reached neon database

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /* config options here */
+    experimental: {
+        instrumentationHook: true,
+    },
+  }
+   
+module.exports = nextConfig
