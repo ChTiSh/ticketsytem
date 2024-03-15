28a082fe9021bc3c4da5782940fda247b0c51b91
ChTiSh <chentina.shen@gmail.com>
2024-03-14 23:55:15 -0700

    log the data

--- src/app/api/ticket-data/route.ts
+++ src/app/api/ticket-data/route.ts
@@ -19,6 +19,7 @@ export async function GET(request:NextRequest, res: NextResponse) {
 
 export async function POST(req:NextRequest, res: NextResponse) {
   const data = await req.json();
+  console.log(data, 'data in apiticketdata'); // Now data will be logged
   const insertedTicket = await db.insert(ticket).values(data)
   console.log('Inserted Ticket:', insertedTicket);  
   return Response.json(data)
@@ -62,38 +63,3 @@ export async function POST(req:NextRequest, res: NextResponse) {
 // }
 
 
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import { db } from '../../../db/index';
-// import { ticket } from '../../../db/ticketTable/schema';
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   const method = req.method;
-
-//   if (method === 'GET') {
-//     try {
-//       // Fetch tickets using your database abstraction
-//       const tickets = await db.select().from(ticket);
-//       res.status(200).json(tickets);
-//     } catch (error) {
-//       console.error('Error fetching tickets:', error);
-//       res.status(500).json({ message: 'Internal Server Error' });
-//     }
-//   } else if (method === 'POST') {
-//     try {
-//       // Insert a new ticket using the parsed request body
-//       const insertedTicket = await db
-//         .insert(ticket)
-//         .values(req.body)
-
-//       res.status(201).json(insertedTicket);
-//     } catch (error) {
-//       console.error('Error creating a ticket:', error);
-//       res.status(500).json({ message: 'Internal Server Error' });
-//     }
-//   } else {
-//     res.status(405).end(`Method Not Allowed`);
-//   }
-// }
